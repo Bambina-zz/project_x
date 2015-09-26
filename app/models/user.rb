c47fc00c21697f15ae3bbc542180d1f527bb7c65
廣野 まゆ子 <mayuko.hirono@gmail.com>
2015-09-26 15:49:38 +0900

    refactor validation codes. #16

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 class User < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,
             format: { with:VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
   ###TODO: ActiveRecord::StatementInvalid例外をキャッチする
   def convert_to_lowercase
-    self.email = email.downcase
+    email.downcase!
   end
 
   has_secure_password
