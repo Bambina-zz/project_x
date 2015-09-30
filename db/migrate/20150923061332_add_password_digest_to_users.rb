d6b0f5a757cdd8db10bcb070d5493681641a83a6
廣野 まゆ子 <mayuko.hirono@gmail.com>
2015-10-01 06:46:36 +0900

    add todo comment #16

--- db/migrate/20150923061332_add_password_digest_to_users.rb
+++ db/migrate/20150923061332_add_password_digest_to_users.rb
@@ -1,6 +1,7 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
     add_column :users, :password_digest, :string
+    ##TODO: Switch the DB to psql, then move NOT NULL constranit into add_column. See #16.
     change_column_null :users, :password_digest, false
   end
 end
