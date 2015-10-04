9b4a45797361ea492626a03b778c3967995f0047
廣野 まゆ子 <mayuko.hirono@gmail.com>
2015-10-04 19:02:40 +0900

    add FactoryGirl and Faker. #12

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,35 +2,30 @@ require 'rails_helper'
 
 describe User do
   it "is valid with a name, email, password and password_confirmation" do
-    user = User.new(
-      name: 'mayu',
-      email: 'a@example.com',
-      password: '123456',
-      password_confirmation: '123456' )
-    expect(user).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it "is invalid without name" do
-    user = User.new( name: nil )
+    user = build( :user, name: nil )
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without email" do
-    user = User.new( email: nil )
+    user = build( :user, email: nil )
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid without password" do
-    user = User.new( password: nil )
+    user = build( :user, password: nil )
     user.valid?
     expect(user.errors[:password]).to include("can't be blank")
   end
 
   it "is invalid without password_confirmation" do
     pending("Add NOT NULL constraint to password_confirmation")
-    user = User.new( password_confirmation: nil )
+    user = build( :user, password_confirmation: nil )
     user.valid?
     expect(user.errors[:password_confirmation]).to include("can't be blank")
   end
@@ -38,20 +33,10 @@ describe User do
   it "is invalid with a duplicate email address" do
     duplicate_email = 'a@example.com'
 
-    User.create(
-      name: 'first',
-      email: duplicate_email,
-      password: '123456',
-      password_confirmation: '123456' )
-
-    user = User.new(
-      name: 'second',
-      email: duplicate_email,
-      password: '123456',
-      password_confirmation: '123456' )
-
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    create( :user, email: duplicate_email )
+    second_user = build( :user, email: duplicate_email )
+    second_user.valid?
+    expect(second_user.errors[:email]).to include("has already been taken")
   end
 
   it "is invalid when password and password_confirmation are not equal" do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,8 +27,8 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # You don't need to specify 'FactoryGirl' for factory_girl methods
+  config.include FactoryGirl::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
