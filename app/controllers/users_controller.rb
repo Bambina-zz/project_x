178c11348fa63720f115b23e0fc6c59ec163f637
廣野 まゆ子 <mayuko.hirono@gmail.com>
2015-10-25 17:37:56 +0900

    refactor codes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(edit show update destroy)
+
   def index
     @users = User.all
   end
@@ -18,16 +20,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to @user
     else
@@ -36,19 +34,21 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id]).destroy
+    @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    redirect_to users_url
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name,
-                                   :email,
-                                   :password,
-                                   :password_confirmation)
-    end
+
+  def set_user
+    @user ||= User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
+  end
 end
