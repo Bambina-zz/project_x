4609e1e0a0bd806722332d26e7fafac0e9ca0b3c
廣野 まゆ子 <mayuko.hirono@gmail.com>
2016-01-02 17:01:54 +0900

    Add feature test for errand management.

---
+++ spec/features/errand_management_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'Errand management', js: true do
+  let!(:user)   { create(:user) }
+  let!(:errand) { create(:errand, owner_id: user.id)}
+
+  before { login_with user.email, user.password }
+
+  scenario 'user is able to create an errand' do
+    fill_in '名前', with: 'Errand 1'
+    expect {
+      click_button '作成する'
+      sleep 0.5
+    }.to change { Errand.count }.by(1)
+  end
+
+  scenario 'user is able to delete an errand' do
+    expect {
+      click_link '削除'
+      sleep 0.5
+    }.to change { Errand.count }.by(-1)
+  end
+end
