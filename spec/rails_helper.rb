ceffd938c8519548ea6b0eb1b4222fd20c3b1f84
廣野 まゆ子 <mayuko.hirono@gmail.com>
2016-01-02 16:54:36 +0900

    Follows DB cleaner’s official settings.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,10 +30,12 @@ RSpec.configure do |config|
   # You don't need to specify 'FactoryGirl' for factory_girl methods
   config.include FactoryGirl::Syntax::Methods
 
+  config.include Helpers
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
